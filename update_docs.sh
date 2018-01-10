0db82a6bb423de60d7686bcbdcb41bfc9689d9cb
Your Name <gessadavide@gmail.com>
2018-01-10 11:32:50 +0100

    bitcoincash splitter

---
+++ update_docs.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cp -r index.html lib/ img/ views/ css/ bower_components/ docs/
+cp js/bitcoincashsplitter.js docs/js
+cp -r index.html lib/ img/ views/ css/ bower_components/ docs/testnet/
+cp js/bitcoincashsplitter.js docs/testnet/js
