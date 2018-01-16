0f938ddc899495885d7c398d2d71cbb1a0603b0d
Your Name <gessadavide@gmail.com>
2018-01-16 16:38:28 +0100

    docs

--- update_docs.sh
+++ update_docs.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 cp -r index.html lib/ img/ views/ css/ bower_components/ docs/
-cp js/bitcoincashsplitter.js docs/js
-cp -r index.html lib/ img/ views/ css/ bower_components/ docs/testnet/
-cp js/bitcoincashsplitter.js docs/testnet/js
+cp js/bitcoincash-splitter.js docs/js
