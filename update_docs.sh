ca6e9c6a0a0c6ffa09de218b26a761eb4114f828
Your Name <gessadavide@gmail.com>
2018-01-17 08:55:19 +0100

    fix deploy

--- update_docs.sh
+++ update_docs.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-cp -r index.html lib/ img/ views/ css/ bower_components/ docs/
-cp js/bitcoincash-splitter.js docs/js
+cp -r index.html lib/ img/ views/ css/ js/ bower_components/ docs/
